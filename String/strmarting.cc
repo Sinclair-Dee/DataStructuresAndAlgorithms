be898162e3343dc74e6d938ca5dec95956e5837e
SinclairDee <dixinkai@outlook.com>
2017-08-20 09:11:46 +0000

    String

---
+++ String/strmarting.cc
@@ -0,0 +1,10 @@
+/*返回子串T在主串S中第pos个字符之后的位置。若不纯在，则函数返回值0.*/
+/*T非空，1<=pos<=strlength(s)*/
+
+int index(String S, String T, int pos){
+    int i = pos; //i用于主串S中当前位置下标，若pos不为1
+    int j = 1;   //j用于子串中当前位置下标
+    while(i <= s[0] && j <= T[0]){//若i小于S长度且j小于T的长度时循环。
+        if(S[i] = T[j])//
+    }
+}
