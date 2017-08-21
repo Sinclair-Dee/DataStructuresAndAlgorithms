d06b27411b08812e1c141f10f42edd8b568c36c6
SinclairDee <dixinkai@outlook.com>
2017-08-21 12:17:44 +0000

    string

---
+++ String/str_kmp.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+/*通过计算返回子串T的next数组。*/
+void get_next(string T, int *next){
+    int i,j;
+    i = 1;
+    j = 0;
+    next[1] = 0;
+    while(i<T.size()){
+        if(j==0 || )
+    }
+}
